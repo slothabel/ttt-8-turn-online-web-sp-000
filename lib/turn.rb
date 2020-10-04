b738f900cecaea42700716ee7106835dfea354a2
Yoshi Mejia <yoshimejia00@gmail.com>
2020-10-04 14:47:18 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,33 @@
+def display_board(board)
+ puts " #{board[0]} | #{board[1]} | #{board[2]} "
+ puts "-----------"
+ puts " #{board[3]} | #{board[4]} | #{board[5]} "
+ puts "-----------"
+ puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board, index) == false
+    true
+  else
+    false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
+end
+
+def move(board, index, token)
+  board[index] = index
+end
